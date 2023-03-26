f75cebf4ad6e03419ad3ce9daa9abcea1279d9b6
ricky-zhf <568058082@qq.com>
2023-03-26 12:15:37 +0800

    update

--- main.go
+++ main.go
@@ -1,13 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type sayer interface { //define a interface
+	say()
+}
+
+//define a struct realizing interface sayer
+type cat struct{}
+type dog struct{}
+
+func (c *cat) say() {
+
+}
+func (d dog) say() {
+
+}
+
+//define a function that can accept the variable implementing the interface sayer
+func hit(arg sayer) {
+	arg.say()
+}
+
+var s1 ss
+
+type ss struct {
+	m map[int]int
+}
+
+var mmm sync.Map
+
+var Mm = map[int]int{
+	1: 1,
+}
 
 func main() {
-	c := make(chan int, 1)
-	x, ok := <-c
-	if ok {
-		fmt.Println(x)
-	} else {
-		fmt.Println("--")
+	ch := make(chan int, 5)
+	go test(ch)
+
+	for {
+		if v, ok := <-ch; ok {
+			fmt.Println("get val: ", v, ok)
+		} else {
+			fmt.Println("done")
+			break
+		}
+	}
+}
+
+func test(ch chan int) {
+	for i := 0; i < 5; i++ {
+		ch <- i
+	}
+
+	time.Sleep(10 * time.Second)
+	close(ch)
+	fmt.Println("put in done")
+}
+
+func t() {
+	for i := 0; i < 10; i++ {
+		go fmt.Println(i)
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("---")
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			fmt.Println(i)
+		}(i)
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("---")
+	for i := 0; i < 20; i++ {
+		go func() {
+			j := i
+			fmt.Println(j)
+		}()
 	}
 }
---
+++ prometheus/metrix.go
@@ -0,0 +1,34 @@
+package prometheus
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"time"
+)
+
+func recordMetrics() {
+	//每2秒，计数器增加1。
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+//公开了 myapp_processed_ops_total 计数器
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func S() {
+	recordMetrics()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
