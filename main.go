21082ddff0a068c41758c195170945c8d1b64273
zhouhuaifeng <568058082@qq.com>
2023-05-21 21:37:27 +0800

    pprof

--- main.go
+++ main.go
@@ -4,21 +4,24 @@ import (
 	"fmt"
 	_ "net/http/pprof"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 var datas []string
 
 func main() {
-	var m map[string]string
-	m["fwef"] = "fwefwe"
-
-	fmt.Println(m["ffwef"])
-
-	str := `{state:{NoUnkeyedLiterals:{} DoNotCompare:[] DoNotCopy:[] atomicMessageInfo:0xc0000302c0} sizeCache:0 unknownFields:[] MsgId:33010000010064821682232416061 Uuid:3301000001006482 ProductCode:NVR EventTime:1682232416 Data:fields:{key:\"alarm_event\"  value:{struct_value:{fields:{key:\"channel\"  value:{number_value:0}}  fields:{key:\"event_id\"  value:{string_value:\"1682232416\"}}  fields:{key:\"event_start\"  value:{string_value:\"1682232416\"}}  fields:{key:\"event_type\"  value:{number_value:8}}  fields:{key:\"report_type\"  value:{number_value:1}}}}} Type:DEVICE_REPORT AppId:121}`
-	fmt.Println(len([]byte(str)))
+	fmt.Println(intSliceToString([]int{1, 2, 3, 3, 3, 3, 3, 1, 2, 3, 3, 4, 4}))
 }
 
+func intSliceToString(intSlice []int) string {
+	stringSlice := make([]string, len(intSlice))
+
+	for i, v := range intSlice {
+		stringSlice[i] = strconv.Itoa(v)
+	}
+	return strings.Join(stringSlice, ",")
+}
 func parseStr(str string, m map[string]interface{}) string {
 	// \$\[([^\]]+)\]|\$\{([^}]+)\}
 	res := regexp.MustCompile(`\$\[([^\]]+)\]|\$\{([^}]+)\}`).FindAllStringSubmatch(str, -1)
